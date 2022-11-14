242d594ef6bbc82c62b0b193a93b0eecc1d45037
Mohamed Saphir <mohamedsaphir@omiraitech.com>
2022-11-14 10:10:54 +0530

    updated render configs for deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
+        #--no-dev --working-dir=/var/www/html/
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
