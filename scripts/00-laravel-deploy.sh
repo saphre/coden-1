b4c6aa6fb880a2fe88efcc3e1d3f780ab2f81645
Mohamed Saphir <mohamedsaphir@omiraitech.com>
2022-11-19 00:00:42 +0530

    updating configs for deployment on Render

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install 
+composer install  --no-dev --working-dir=/var/www/html/
 
 echo "Caching config..."
 php artisan config:cache
